b2d9cab45e4cd04783fe66efec1d67418e0e9e61
Lukas Tanner <schrot@outlook.com>
2016-06-09 12:06:08 +0200

    implement GetChanges

--- Tacs20ImportClient/ApiClient.cs
+++ Tacs20ImportClient/ApiClient.cs
@@ -21,9 +21,14 @@ namespace Tacs20ImportClient
         #endregion
 
         #region public methods
+        /// <summary>
+        /// Hier wird demonstriert, die der initiale Import funktionieren kann. Dies muss nur einmal nach
+        /// der ersten Konfiguration des Mandanten geschehen, oder wenn die ganze Konfiguration neu 
+        /// eingelesen werden soll.
+        /// </summary>
+        /// <returns>Ein Task für die Asynchronität</returns>
         public async Task GetCompleteImport()
         {
-            Console.WriteLine("--- get complete import ---" + Environment.NewLine);
             var tokenResponse = await GetToken();
             using (var client = GetClient(tokenResponse))
             {
@@ -38,6 +43,8 @@ namespace Tacs20ImportClient
                     var nutzniesser = GetCollection<Nutzniesser>(baseNavigation.NutzniesserUrl);
                     var variablen = GetCollection<Variable>(baseNavigation.VariablenUrl);
                     var organisations = GetCollection<Organisation>(baseNavigation.OrganisationUrl);
+
+                    // Die Daten des Grundkatalogs speichern
                     SaveData(await nutzniesser);
                     SaveData(await variablen);
                     SaveData(await statistikCodes);
@@ -48,6 +55,50 @@ namespace Tacs20ImportClient
 
                     // Zuweisungen der Variablen, Nutzniesser und Statistikcode zu einzelnen Anstellungen abholen
                     ProcessAnstellungen(baseNavigation.AnstellungLink);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Hier wird demonstriert, wie die Aktualisierung der Daten funktionieren kann. Dies ist der 
+        /// Normalfall.
+        /// </summary>
+        /// <param name="changesSince">Das Datum des letzten Imports.</param>
+        /// <returns>Ein Task für die Asynchronität</returns>
+        public async Task GetChanges(DateTime changesSince)
+        {
+            var tokenResponse = await GetToken();
+            using (var client = GetClient(tokenResponse))
+            {
+                var response = await client.GetAsync("api/v1");
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    var baseNavigation = JsonConvert.DeserializeObject<BaseNavigation>(content);
+
+                    // Den URLs den Query-Parameter changesSince hinzufügen
+                    string statistikCodeUrl = AddChangesSince(baseNavigation.StatistikCodeUrl, changesSince);
+                    string nutzniesserUrl = AddChangesSince(baseNavigation.NutzniesserUrl, changesSince);
+                    string variablenUrl = AddChangesSince(baseNavigation.VariablenUrl, changesSince);
+                    string organisationsUrl = AddChangesSince(baseNavigation.OrganisationUrl, changesSince);
+
+                    // Mit bestimmten URLs der BaseNavigation die Änderungen des Grundkatalogs abholen
+                    var statistikCodes = GetCollection<StatistikCodeImport>(statistikCodeUrl);
+                    var nutzniesser = GetCollection<Nutzniesser>(nutzniesserUrl);
+                    var variablen = GetCollection<Variable>(variablenUrl);
+                    var organisations = GetCollection<Organisation>(organisationsUrl);
+
+                    // Die alten Daten löschen (oder deaktivieren) und mit den Änderungen ersetzen
+                    DeleteAndSave(await statistikCodes);
+                    DeleteAndSave(await nutzniesser);
+                    DeleteAndSave(await variablen);
+                    DeleteAndSave(await organisations);
+
+                    // Zuweisungen der Variablen, Nutzniesser und Statistikcodes zu den Organisationen abholen
+                    ProcessOrganisations(await organisations, changesSince);
+
+                    // Zuweisungen der Variablen, Nutzniesser und Statistikcode zu einzelnen Anstellungen abholen
+                    ProcessAnstellungen(baseNavigation.AnstellungLink, changesSince);
                 }
             }
         }
@@ -63,15 +114,45 @@ namespace Tacs20ImportClient
         {
             foreach (var organisation in organisations)
             {
-                var variablenRef = await GetCollection<VariablenRef>(organisation.VariablenSetUrl);
-                var statistikCodeRef = await GetCollection<StatistikCodeRef>(organisation.StatistikCodeUrl);
-                var nutzniesserRef = await GetCollection<NutzniesserRef>(organisation.NutzniesserUrl);
-                SaveData(variablenRef, organisation.OrganisationId);
-                SaveData(statistikCodeRef, organisation.OrganisationId);
-                SaveData(nutzniesserRef, organisation.OrganisationId);
+                var variablenRef = GetCollection<VariablenRef>(organisation.VariablenSetUrl);
+                var statistikCodeRef = GetCollection<StatistikCodeRef>(organisation.StatistikCodeUrl);
+                var nutzniesserRef = GetCollection<NutzniesserRef>(organisation.NutzniesserUrl);
+                SaveData(await variablenRef, organisation.OrganisationId);
+                SaveData(await statistikCodeRef, organisation.OrganisationId);
+                SaveData(await nutzniesserRef, organisation.OrganisationId);
 
                 // Zuweisungen zu Personalkategorien innerhalb einer Organisation abholen
                 ProcessPersonalkategorien(organisation.PersonalkategorieUrl, organisation.OrganisationId);
+            }
+        }
+
+        /// <summary>
+        /// Holt alle Änderungen der Zuweisungen der Variablen, Nutzniesser und Statistikcodes zu den 
+        /// Organisationen ab und speichert sie
+        /// </summary>
+        /// <param name="organisations">Die Organisationen, dessen Zuweisungen abgeholt werden sollen</param>
+        /// <param name="changesSince">Das Datum der letzten Synchronisation</param>
+        private async void ProcessOrganisations(IEnumerable<Organisation> organisations, DateTime changesSince)
+        {
+            foreach (var organisation in organisations)
+            {
+                // Den URLs den Query-Parameter changesSince hinzufügen
+                var variablenUrl = AddChangesSince(organisation.VariablenSetUrl, changesSince);
+                var statistikCodeUrl = AddChangesSince(organisation.StatistikCodeUrl, changesSince);
+                var nutzniesserUrl = AddChangesSince(organisation.NutzniesserUrl, changesSince);
+
+                // Die Änderungen seit changesSince abholen
+                var variablenRef = GetCollection<VariablenRef>(variablenUrl);
+                var nutzniesserRef = GetCollection<NutzniesserRef>(nutzniesserUrl);
+                var statistikCodeRef = GetCollection<StatistikCodeRef>(statistikCodeUrl);
+
+                // Die alten Daten löschen (oder deaktivieren) und mit den Änderungen ersetzen
+                DeleteAndSave(await variablenRef, organisation.OrganisationId);
+                DeleteAndSave(await nutzniesserRef, organisation.OrganisationId);
+                DeleteAndSave(await statistikCodeRef, organisation.OrganisationId);
+
+                // Zuweisungen zu Personalkategorien innerhalb einer Organisation abholen
+                ProcessPersonalkategorien(organisation.PersonalkategorieUrl, organisation.OrganisationId, changesSince);
             }
         }
 
@@ -88,12 +169,41 @@ namespace Tacs20ImportClient
             var persKat = await GetCollection<PersonalkategorieNav>(personalkategorieUrl);
             foreach (var personalkategorieNav in persKat)
             {
-                var variablenRef = await GetCollection<VariablenRef>(personalkategorieNav.VariablenUrl);
-                var nutzniesserRef = await GetCollection<NutzniesserRef>(personalkategorieNav.NutzniesserUrl);
-                var statistikCodeRef = await GetCollection<StatistikCodeRef>(personalkategorieNav.StatistikCodeUrl);
-                SaveData(variablenRef, organisationId, personalkategorieNav.PersonalkategorieId);
-                SaveData(nutzniesserRef, organisationId, personalkategorieNav.PersonalkategorieId);
-                SaveData(statistikCodeRef, organisationId, personalkategorieNav.PersonalkategorieId);
+                var variablenRef = GetCollection<VariablenRef>(personalkategorieNav.VariablenUrl);
+                var nutzniesserRef = GetCollection<NutzniesserRef>(personalkategorieNav.NutzniesserUrl);
+                var statistikCodeRef = GetCollection<StatistikCodeRef>(personalkategorieNav.StatistikCodeUrl);
+                SaveData(await variablenRef, organisationId, personalkategorieNav.PersonalkategorieId);
+                SaveData(await nutzniesserRef, organisationId, personalkategorieNav.PersonalkategorieId);
+                SaveData(await statistikCodeRef, organisationId, personalkategorieNav.PersonalkategorieId);
+            }
+        }
+
+        /// <summary>
+        /// Holt alle Änderungen der Zuweisungen der Variablen, Nutzniesser und Statistikcodes zu den 
+        /// Personalkategorien (-typ, -gruppe) und speichert sie
+        /// </summary>
+        /// <param name="personalkategorieUrl">Die URL, unter welcher die Personalkategorien abgeholt 
+        /// werden können</param>
+        /// <param name="organisationId">Der tacs-Code der Organisation, zu welcher die Personalkategorien gehören</param>
+        /// <param name="changesSince">Das Datum der letzten Synchronisation</param>
+        private async void ProcessPersonalkategorien(string personalkategorieUrl, string organisationId, DateTime changesSince)
+        {
+            personalkategorieUrl = AddChangesSince(personalkategorieUrl, changesSince);
+            var persKat = await GetCollection<PersonalkategorieNav>(personalkategorieUrl);
+
+            foreach (var nav in persKat)
+            {
+                var variablenUrl = AddChangesSince(nav.VariablenUrl, changesSince);
+                var nutzniesserUrl = AddChangesSince(nav.NutzniesserUrl, changesSince);
+                var statistikCodeUrl = AddChangesSince(nav.StatistikCodeUrl, changesSince);
+
+                var variablenRef = GetCollection<VariablenRef>(variablenUrl);
+                var nutzniesserRef = GetCollection<NutzniesserRef>(nutzniesserUrl);
+                var statistikCode = GetCollection<StatistikCodeRef>(statistikCodeUrl);
+
+                DeleteAndSave(await variablenRef, organisationId, nav.PersonalkategorieId);
+                DeleteAndSave(await nutzniesserRef, organisationId, nav.PersonalkategorieId);
+                DeleteAndSave(await statistikCode, organisationId, nav.PersonalkategorieId);
             }
         }
 
@@ -116,6 +226,33 @@ namespace Tacs20ImportClient
                 SaveData(variablenRef, anstellung.AnstellungsId);
                 SaveData(nutzniesserRef, anstellung.AnstellungsId);
                 SaveData(statistikCodeRef, anstellung.AnstellungsId);
+            }
+        }
+
+        /// <summary>
+        /// Holt alle Änderungen der Zuweisungen der Variablen, Nutzniesser und Statistikcodes, die nur 
+        /// für einzelne Anstellungen gelten und speichert sie
+        /// </summary>
+        /// <param name="anstellungLink">Die URL unter welcher die Anstellungen mit separaten Zuweisungen 
+        /// zu finden sind</param>
+        /// <param name="changesSince">Das Datum der letzten Synchronisation</param>
+        private async void ProcessAnstellungen(string anstellungLink, DateTime changesSince)
+        {
+            anstellungLink = AddChangesSince(anstellungLink, changesSince);
+            var anstellungen = await GetCollection<Anstellung>(anstellungLink);
+            foreach (var anstellung in anstellungen)
+            {
+                var variablenUrl = AddChangesSince(anstellung.VariablenUrl, changesSince);
+                var nutzniesserUrl = AddChangesSince(anstellung.NutzniesserUrl, changesSince);
+                var statistikCodeUrl = AddChangesSince(anstellung.StatistikCodeUrl, changesSince);
+
+                var variablenRef = GetCollection<VariablenRef>(variablenUrl);
+                var nutzniesserRef = GetCollection<NutzniesserRef>(nutzniesserUrl);
+                var statistikCodeRef = GetCollection<StatistikCodeRef>(statistikCodeUrl);
+
+                DeleteAndSave(await variablenRef, anstellung.AnstellungsId);
+                DeleteAndSave(await nutzniesserRef, anstellung.AnstellungsId);
+                DeleteAndSave(await statistikCodeRef, anstellung.AnstellungsId);
             }
         }
 
@@ -158,55 +295,33 @@ namespace Tacs20ImportClient
         /// Diese Zusammenlegung  wurde aus Bequemlichkeit gewählt</param>
         /// <param name="personalKategorieId">Wird verwendet, wenn es sich um Zuweisungen zu Personalkategorien
         /// handelt, die gespeichert werden sollen</param>
-        private static void SaveData<T>(IEnumerable<T> data, string organisationOrAnstellungId = null, 
+        private static void SaveData<T>(IEnumerable<T> data, string organisationOrAnstellungId = null,
                                         string personalKategorieId = null)
         {
             // Hier können die Daten gespeichert werden.
         }
 
-        private async void SaveCollection<T>(string url)
-        {
-            TokenResponse tokenResponse = await GetToken();
-            using (HttpClient client = GetClient(tokenResponse))
-            {
-                var response = await client.GetAsync(url);
-                // Wenn die URL korrekt ist, aber keine Daten vorhanden sind, returniert der Server 204
-                if (response.IsSuccessStatusCode && response.StatusCode != HttpStatusCode.NoContent)
-                {
-                    string content = await response.Content.ReadAsStringAsync();
-                    IEnumerable<T> result = JsonConvert.DeserializeObject<IEnumerable<T>>(content);
-                    // Hier können die Daten gespeichert werden
 
-                    string length = GetContentLength(response);
-                    Console.WriteLine();
-                    Console.WriteLine($"{result.Count()} {typeof(T).Name} geholt");
-                    Console.WriteLine($"dies sind {length} bytes");
-                }
-            }
+        /// <summary>
+        /// Stub-Methode, wo die aktualisierten Daten gespeichert werden können
+        /// </summary>
+        /// <typeparam name="T">Der Type der Daten, die gespeichert werden sollen</typeparam>
+        /// <param name="data">Die Liste mit den Daten, die gespeichert werden sollen</param>
+        /// <param name="organisationOrAnstellungId">Wird verwendet, wenn es sich um Zuweisungen zu 
+        /// Organisationen oder Anstellungen handelt, die gespeichert werden sollen.
+        /// Diese Zusammenlegung  wurde aus Bequemlichkeit gewählt</param>
+        /// <param name="personalKategorieId">Wird verwendet, wenn es sich um Zuweisungen zu Personalkategorien
+        /// handelt, die gespeichert werden sollen</param>
+        private static void DeleteAndSave<T>(IEnumerable<T> data, string organisationOrAnstellungId = null,
+                                      string personalKategorieId = null)
+        {
+            // Hier können die Daten, die aktualisiert werden sollen, gelöscht oder deaktiviert 
+            // und die neuen gespeichert werden
         }
 
-        private async Task<IEnumerable<T>> SaveAndReturnCollection<T>(string url)
+        private static string AddChangesSince(string statistikCodeUrl, DateTime changesSince)
         {
-            TokenResponse tokenResponse = await GetToken();
-            using (var client = GetClient(tokenResponse))
-            {
-                var response = await client.GetAsync(url);
-                IEnumerable<T> result = new List<T>();
-                if (response.IsSuccessStatusCode && response.StatusCode != HttpStatusCode.NoContent)
-                {
-                    string content = await response.Content.ReadAsStringAsync();
-                    result = JsonConvert.DeserializeObject<IEnumerable<T>>(content);
-                    // here you can save the data
-
-                    string length = GetContentLength(response);
-                    Console.WriteLine();
-                    Console.WriteLine($"{result.Count()} Organisationen geholt");
-                    Console.WriteLine($"dies sind {length} bytes");
-                }
-
-                return result;
-            }
-
+            return string.Concat(statistikCodeUrl, "?changesSince=", changesSince.ToString("yyyy-MM-dd"));
         }
 
         private async Task<TokenResponse> GetToken()
@@ -237,7 +352,7 @@ namespace Tacs20ImportClient
             return _currentToken;
         }
 
-        private HttpClient GetClient(TokenResponse token)
+        private static HttpClient GetClient(TokenResponse token)
         {
             string accessToken = token.AccessToken;
             HttpClient client = new HttpClient();
--- Tacs20ImportClient/Program.cs
+++ Tacs20ImportClient/Program.cs
@@ -11,7 +11,14 @@ namespace Tacs20ImportClient
         static void Main(string[] args)
         {
             ApiClient client = new ApiClient();
+            Console.WriteLine("--- get complete import ---" + Environment.NewLine);
             client.GetCompleteImport().Wait();
+            Console.WriteLine("--- initial import completed ---" + Environment.NewLine);
+
+            DateTime changesSince = new DateTime(2016, 05, 15);
+            Console.WriteLine($"--- get changes since {changesSince} ---" + Environment.NewLine);
+            client.GetChanges(changesSince).Wait();
+            Console.WriteLine("--- import of changes completed ---" + Environment.NewLine);
 
             Console.ReadKey();
         }
